2980ea0489f594c081209a8a380a902aaf547a14
Evan Kennedy <evanckennedy@protonmail.com>
2024-05-02 13:54:21 -0500

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+function Header() {
+  return (
+    <header>
+      <div className="container flex justify-between">
+        <h1>StarCineplex</h1>
+        <nav>
+          <ul className="flex gap-1">
+            <li>Showtimes</li>
+            <li>Tickets</li>
+            <li>Contact</li>
+          </ul>
+        </nav>  
+      </div>
+    </header>
+  )
+}
+
+export default Header;
